167ec87f3cad129804ffc65d7005a56daf796df0
FirdousArshiya <firdousarshiya99@gmail.com>
2024-02-09 12:29:08 +0000

    jobby application

---
+++ src/components/Jobs/index.js
@@ -0,0 +1,241 @@
+import {Component} from 'react'
+import Cookies from 'js-cookie'
+import Loader from 'react-loader-spinner'
+import {BsSearch} from 'react-icons/bs'
+import FiltersGroup from '../FiltersGroup'
+import JobCard from '../JobCard'
+import Header from '../Header'
+
+import './index.css'
+
+const employmentTypesList = [
+  {
+    label: 'Full Time',
+    employmentTypeId: 'FULLTIME',
+  },
+  {
+    label: 'Part Time',
+    employmentTypeId: 'PARTTIME',
+  },
+  {
+    label: 'Freelance',
+    employmentTypeId: 'FREELANCE',
+  },
+  {
+    label: 'Internship',
+    employmentTypeId: 'INTERNSHIP',
+  },
+]
+
+const salaryRangesList = [
+  {
+    salaryRangeId: '1000000',
+    label: '10 LPA and above',
+  },
+  {
+    salaryRangeId: '2000000',
+    label: '20 LPA and above',
+  },
+  {
+    salaryRangeId: '3000000',
+    label: '30 LPA and above',
+  },
+  {
+    salaryRangeId: '4000000',
+    label: '40 LPA and above',
+  },
+]
+
+const apiStatusConstants = {
+  initial: 'INITIAL',
+  success: 'SUCCESS',
+  failure: 'FAILURE',
+  inProgress: 'IN_PROGRESS',
+}
+
+class Jobs extends Component {
+  state = {
+    jobsList: [],
+    apiStatus: apiStatusConstants.initial,
+    searchInput: '',
+    employmentType: [],
+    minimumSalary: 0,
+  }
+
+  componentDidMount() {
+    this.getJobs()
+  }
+
+  getJobs = async () => {
+    this.setState({apiStatus: apiStatusConstants.inProgress})
+
+    const {employmentType, minimumSalary, searchInput} = this.state
+    const apiUrl = `https://apis.ccbp.in/jobs?employment_type=${employmentType.join()}&minimum_package=${minimumSalary}&search=${searchInput}`
+
+    const jwtToken = Cookies.get('jwt_token')
+    const options = {
+      headers: {
+        Authorization: `Bearer ${jwtToken}`,
+      },
+      method: 'GET',
+    }
+
+    const response = await fetch(apiUrl, options)
+    if (response.ok === true) {
+      const data = await response.json()
+      const updatedData = data.jobs.map(eachJob => ({
+        companyLogoUrl: eachJob.company_logo_url,
+        employmentType: eachJob.employment_type,
+        id: eachJob.id,
+        jobDescription: eachJob.job_description,
+        location: eachJob.location,
+        packagePerAnnum: eachJob.package_per_annum,
+        rating: eachJob.rating,
+        title: eachJob.title,
+      }))
+      this.setState({
+        jobsList: updatedData,
+        apiStatus: apiStatusConstants.success,
+      })
+    } else {
+      this.setState({apiStatus: apiStatusConstants.failure})
+    }
+  }
+
+  renderJobsListView = () => {
+    const {jobsList} = this.state
+    const shouldShowJobsList = jobsList.length > 0
+
+    return shouldShowJobsList ? (
+      <div className="all-jobs-container">
+        <ul className="jobs-list">
+          {jobsList.map(eachJob => (
+            <JobCard jobDetails={eachJob} key={eachJob.id} />
+          ))}
+        </ul>
+      </div>
+    ) : (
+      <div className="no-jobs-view">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/no-jobs-img.png"
+          alt="no jobs"
+          className="no-jobs-img"
+        />
+        <h1 className="no-jobs-headings">No Jobs Found</h1>
+        <p className="no-jobs-description">
+          We could not find any jobs. Try other filters.
+        </p>
+      </div>
+    )
+  }
+
+  renderFailureView = () => (
+    <div className="jobs-error-view-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/failure-img.png"
+        alt="failure view"
+        className="jobs-failure-img"
+      />
+      <h1 className="jobs-failure-heading-text">Oops! Something Went Wrong</h1>
+      <p className="jobs-failure-description">
+        We cannot seem to find the page you are looking for
+      </p>
+      <button
+        type="button"
+        onClick={this.getJobs}
+        data-testid="button"
+        className="jobs-failure-button"
+      >
+        Retry
+      </button>
+    </div>
+  )
+
+  renderLoadingView = () => (
+    <div className="loader-container" data-testid="loader">
+      <Loader type="ThreeDots" color="#ffffff" height="50" width="50" />
+    </div>
+  )
+
+  renderBasedOnApiStatus = () => {
+    const {apiStatus} = this.state
+
+    switch (apiStatus) {
+      case apiStatusConstants.success:
+        return this.renderJobsListView()
+      case apiStatusConstants.failure:
+        return this.renderFailureView()
+      case apiStatusConstants.inProgress:
+        return this.renderLoadingView()
+      default:
+        return null
+    }
+  }
+
+  changeSearchInput = event => {
+    this.setState({searchInput: event.target.value})
+  }
+
+  onEnterSearchInput = event => {
+    if (event.key === 'Enter') {
+      this.getJobs()
+    }
+  }
+
+  changeSalary = salary => {
+    this.setState({minimumSalary: salary}, this.getJobs)
+  }
+
+  changeEmploymentType = type => {
+    this.setState(
+      prevState => ({
+        employmentType: [...prevState.employmentType, type],
+      }),
+      this.getJobs,
+    )
+  }
+
+  render() {
+    const {searchInput} = this.state
+    return (
+      <>
+        <Header />
+        <div className="jobs-container">
+          <div className="jobs-content">
+            <FiltersGroup
+              employmentTypesList={employmentTypesList}
+              salaryRangesList={salaryRangesList}
+              changeSearchInput={this.changeSearchInput}
+              changeSalary={this.changeSalary}
+              changeEmploymentType={this.changeEmploymentType}
+              searchInput={searchInput}
+              getJobs={this.getJobs}
+            />
+            <div className="search-input-jobs-list-container">
+              <div className="search-input-container-desktop">
+                <input
+                  type="search"
+                  placeholder="Search"
+                  className="search-input-desktop"
+                  onChange={this.changeSearchInput}
+                  onKeyDown={this.onEnterSearchInput}
+                />
+                <button
+                  type="button"
+                  onClick={this.getJobs}
+                  data-testid="searchButton"
+                  className="search-button-container-desktop"
+                  aria-label="close"
+                >
+                  <BsSearch className="search-icon-desktop" />
+                </button>
+              </div>
+              {this.renderBasedOnApiStatus()}
+            </div>
+          </div>
+        </div>
+      </>
+    )
+  }
+}
+export default Jobs
